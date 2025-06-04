18fcc86fdbfca6671cea897ad29f1fb6a6eaf1d2
alyssialopr <alyssia.pradon@gmail.com>
2025-06-04 15:43:09 +0200

    index + visu des livres

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ import bookRouter from "./routes/books.js";
 import libraryRouter from "./routes/library.js"; 
 import customerRouter from "./routes/customers.js";
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 dotenv.config();
 connectDB();
 
