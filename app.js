6451372dbbf68cc8ff696577d69cb0d8cdfde689
alyssialopr <alyssia.pradon@gmail.com>
2025-06-04 15:55:46 +0200

    rectification errors

--- app.js
+++ app.js
@@ -9,15 +9,22 @@ import bookRouter from "./routes/books.js";
 import libraryRouter from "./routes/library.js"; 
 import customerRouter from "./routes/customers.js";
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 dotenv.config();
 connectDB();
+
 
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/", indexRouter);
